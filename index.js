7e5edf002ee7da09835cde944fd8d4551851abf4
unknown <rgarcia20.m@gmail.com>
2023-05-11 08:12:46 -0700

    code to import the inquirer package

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 const fs = require('fs');
 const {isValidColor} = require("./lib/color");
 const LogoMaker = require("./lib/logoMaker");
+
+
+(async () => {
+    
+    // importing the inquirer package
+    const {default: inquirer} = await import('inquirer');
+
+})
