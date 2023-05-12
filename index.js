95275c7e351798eb9ac0c63979bf1a2fef7e7bd8
saway11 <rgarcia20.m@gmail.com>
2023-05-12 14:22:06 -0700

    added more questions for user and created a prompt module using inquirer.createpromptModule

--- index.js
+++ index.js
@@ -16,5 +16,27 @@ const questions = [
             }
             return true;
         }
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (keyword or hexadecimal number)'
+    },
+    {
+        type: 'list',
+        name: 'shapeType',
+        message: 'Select a shape',
+        choices: ['Triangle', 'Circle', 'Square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (keyword or hexadecimal number)'
     }
-]
+];
+
+// Created a prompt module using the inquirer.createPromptModule and passes
+// the questions array as an argument. 
+inquirer.createPromptModule(questions).then(({text, textColor, shapeType, shapeColor}) => {
+    let shape;
+})
